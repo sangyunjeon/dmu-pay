653b5776697e61d454b6d923a6cea2768458b389
Sangyun Jeon <jsycd0130@naver.com>
2025-06-03 00:11:52 +0900

    반응형 추가

--- src/pages/StudentMainPage.js
+++ src/pages/StudentMainPage.js
@@ -1,11 +1,15 @@
 import Header from '../components/Header';
+import './StudentMainPage.css';
 
 function StudentMainPage() {
   return (
-    <div>
+    <>
       <Header />
-      {/* 나머지 학생용 콘텐츠 */}
-    </div>
+      <div className="student-main">
+        <h2>안녕하세요, 학생님 👋</h2>
+        <p>이곳에서 포인트를 확인하고, 가맹점 정보를 조회할 수 있어요.</p>
+      </div>
+    </>
   );
 }
 
