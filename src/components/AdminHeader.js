f578db0e9860d37a7e18ccc4a0ece538d9e1387a
Sangyun Jeon <jsycd0130@naver.com>
2025-06-12 02:13:57 +0900

    hh

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./AdminHeader.css";
 
 function AdminHeader() {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    alert("로그아웃 되었습니다.");
-    navigate("/");
-  };
-
   return (
     <header className="admin-header">
       <div className="admin-header-left">
@@ -18,10 +10,7 @@ function AdminHeader() {
       </div>
 
       <div className="admin-header-right">
-        <span className="admin-name">Admin 님</span>
-        <button className="admin-logout" onClick={handleLogout}>
-          로그아웃
-        </button>
+        Admin 000님 | 로그아웃
       </div>
     </header>
   );
