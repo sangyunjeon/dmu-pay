c6161b55320b2a743049e6365613012c9292eada
Sangyun Jeon <jsycd0130@naver.com>
2025-06-12 02:41:18 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import StudentMainPage from "./pages/StudentMainPage";
 import MerchantMainPage from "./pages/MerchantMainPage";
@@ -13,11 +13,19 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        {/* "/" 접근 시 자동으로 "/login"으로 리디렉트 */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* 로그인 관련 */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+
+        {/* 메인 페이지들 */}
         <Route path="/student" element={<StudentMainPage />} />
         <Route path="/merchant" element={<MerchantMainPage />} />
         <Route path="/admin" element={<AdminMainPage />} />
+
+        {/* 서브 페이지들 */}
         <Route path="/point-history" element={<PointHistoryPage />} />
         <Route path="/ranking" element={<RankingPage />} />
         <Route path="/merchants" element={<MerchantInfoPage />} />
