b4fbc1e6a6bc87a578656d8f9504a2d1585f7922
Sangyun Jeon <jsycd0130@naver.com>
2025-07-19 12:43:11 +0900

    test

--- src/pages/MerchantMainPage.js
+++ src/pages/MerchantMainPage.js
@@ -4,11 +4,12 @@ import React from 'react';
 
 function MerchantMainPage() {
   return (
-    <div className="merchant-main-page">
+    <div style={{ backgroundColor: "#f0f0f0", padding: "40px", minHeight: "100vh", color: "#000" }}>
       <h1>가맹점 메인 페이지</h1>
       <p>여기에 가맹점 전용 기능들이 들어갈 예정입니다.</p>
     </div>
   );
 }
+
 
 export default MerchantMainPage;
