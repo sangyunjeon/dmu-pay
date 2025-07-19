81d0084cf702f662ded65a947ba491414be4403c
Sangyun Jeon <jsycd0130@naver.com>
2025-07-19 12:39:33 +0900

    asd

---
+++ src/pages/MerchantMainPage.js
@@ -0,0 +1,14 @@
+// src/pages/MerchantMainPage.js
+
+import React from 'react';
+
+function MerchantMainPage() {
+  return (
+    <div className="merchant-main-page">
+      <h1>가맹점 메인 페이지</h1>
+      <p>여기에 가맹점 전용 기능들이 들어갈 예정입니다.</p>
+    </div>
+  );
+}
+
+export default MerchantMainPage;
