e1a76a3f22cef2ea17001af1cadf038e84867966
Sangyun Jeon <jsycd0130@naver.com>
2025-07-19 11:19:23 +0900

    rename

--- src/pages/MerchantEditPage.js
+++ src/pages/MerchantEditPage.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import './MerchantRegisterPage.css'; // 기존 스타일 재사용
+import './AdminMerchantRegisterPage.css'; // 기존 스타일 재사용
 import AdminHeader from '../components/AdminHeader';
 import AdminSidebar from '../components/AdminSidebar';
 
