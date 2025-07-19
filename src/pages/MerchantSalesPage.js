be439810ef0d3d5b496e67700c900b6c0a8b5cbc
Sangyun Jeon <jsycd0130@naver.com>
2025-07-19 13:20:57 +0900

    asd

--- src/pages/MerchantSalesPage.js
+++ src/pages/MerchantSalesPage.js
@@ -1,5 +1,24 @@
 // src/pages/MerchantSalesPage.js
+
+import React from "react";
+import MerchantHeader from "../components/MerchantHeader"; // 헤더 import
+
 function MerchantSalesPage() {
-  return <h1>📊 매출현황 페이지</h1>;
+  const handleLogout = () => {
+    sessionStorage.clear();
+    window.location.href = "/login";
+  };
+
+  return (
+    <div>
+      <MerchantHeader
+        storeName="가게 이름"
+        owner="000 사장님"
+        onLogout={handleLogout}
+      />
+      <h1>📊 매출현황 페이지</h1>
+    </div>
+  );
 }
+
 export default MerchantSalesPage;
